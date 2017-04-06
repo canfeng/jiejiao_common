af336c0be21d6f2f3709f39975a4f6eb880d686f
canfeng <stf91819@163.com>
2017-04-06 11:37:53 +0800

    md5工具类修改，修改中文加密不一致的情况

--- src/main/java/com/jiejiao/common/utils/security/Md5Util.java
+++ src/main/java/com/jiejiao/common/utils/security/Md5Util.java
@@ -10,6 +10,38 @@ import com.jiejiao.common.tencent.RandomStringGenerator;
  *
  */
 public class Md5Util {
+	
+	  private final static String[] hexDigits = {"0", "1", "2", "3", "4", "5", "6", "7",
+	            "8", "9", "a", "b", "c", "d", "e", "f"};
+
+	
+	 /**
+     * 转换字节数组为16进制字串
+     * @param b 字节数组
+     * @return 16进制字串
+     */
+    public static String byteArrayToHexString(byte[] b) {
+        StringBuilder resultSb = new StringBuilder();
+        for (byte aB : b) {
+            resultSb.append(byteToHexString(aB));
+        }
+        return resultSb.toString();
+    }
+    
+    /**
+     * 转换byte到16进制
+     * @param b 要转换的byte
+     * @return 16进制格式
+     */
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0) {
+            n = 256 + n;
+        }
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
 
 	/**
 	 * md5加密
@@ -20,7 +52,7 @@ public class Md5Util {
 	 */
 	public static String getMD5(String input) {
 
-		MessageDigest md5 = null;
+		/*MessageDigest md5 = null;
 		try {
 			md5 = MessageDigest.getInstance("MD5");
 		} catch (Exception e) {
@@ -42,7 +74,17 @@ public class Md5Util {
 			}
 			hexValue.append(Integer.toHexString(val));
 		}
-		return hexValue.toString();
+		return hexValue.toString();*/
+		
+		String resultString = null;
+        try {
+            resultString = origin;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultString;
 	}
 	
 	/**

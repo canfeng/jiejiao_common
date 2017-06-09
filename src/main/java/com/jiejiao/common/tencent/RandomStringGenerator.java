8bfc51aa09a622edb8c12cf7406d0594375f2591
canfeng <stf91819@163.com>
2017-06-09 19:40:01 +0800

    增加随机生成数字、大写、小写字母组合的字符串

--- src/main/java/com/jiejiao/common/tencent/RandomStringGenerator.java
+++ src/main/java/com/jiejiao/common/tencent/RandomStringGenerator.java
@@ -26,6 +26,22 @@ public class RandomStringGenerator {
     }
     
     /**
+     * 获取一定长度的随机字符串（包括大写字母）
+     * @param length 指定字符串长度
+     * @return 一定长度的字符串
+     */
+    public static String getRandomStringHasUpperCharactorByLength(int length) {
+    	String baseStr = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    	Random random = new Random();
+    	StringBuffer sb = new StringBuffer();
+    	for (int i = 0; i < length; i++) {
+    		int number = random.nextInt(baseStr.length());
+    		sb.append(baseStr.charAt(number));
+    	}
+    	return sb.toString();
+    }
+    
+    /**
      * 获取一定长度的随机数（纯数字）
      * @author shizhiguo
      * @date 2017年4月6日 下午5:13:22
